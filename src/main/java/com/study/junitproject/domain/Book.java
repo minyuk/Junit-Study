95aac9a399b9206832cb26d62b3f510b400dbe47
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-10-04 17:23:14 +0900

    create entity

--- src/main/java/com/study/junitproject/domain/Book.java
+++ src/main/java/com/study/junitproject/domain/Book.java
@@ -1,4 +1,36 @@
 package com.study.junitproject.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 20, nullable = false)
+    private String author;
+
+    @Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
 }
