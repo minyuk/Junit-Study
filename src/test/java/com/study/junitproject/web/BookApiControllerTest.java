83d14f726d617685f15406c44dfd406e940a9ca6
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-10-21 16:46:17 +0900

    Controller Integration Test - getList Book

--- src/test/java/com/study/junitproject/web/BookApiControllerTest.java
+++ src/test/java/com/study/junitproject/web/BookApiControllerTest.java
@@ -3,21 +3,27 @@ package com.study.junitproject.web;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
-import com.study.junitproject.service.BookService;
+import com.study.junitproject.domain.Book;
+import com.study.junitproject.domain.BookRepository;
 import com.study.junitproject.web.dto.request.BookSaveRequestDto;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BookApiControllerTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
     @Autowired
     private TestRestTemplate rt;
 
@@ -29,6 +35,16 @@ public class BookApiControllerTest {
         om = new ObjectMapper();
         headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @BeforeEach // 각 테스트 시작전에 한번씩 실행
+    public void createBook() {
+        Book book = Book.builder()
+                .title("Junit")
+                .author("겟인데어")
+                .build();
+
+        bookRepository.save(book);
     }
 
     @Test
@@ -52,6 +68,27 @@ public class BookApiControllerTest {
 
         assertThat(title).isEqualTo("Testing...");
         assertThat(author).isEqualTo("Tester");
+    }
+    
+    @Test
+    @DisplayName("책 목록보기")
+    @Sql("classpath:db/tableInit.sql")
+    void getList() throws Exception {
+        //given
+        
+        //when
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = rt.exchange("/api/v1/book", HttpMethod.GET, request, String.class);
+
+        //then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        Integer code = dc.read("$.code");
+        String title = dc.read("$.body.items[0].title");
+        String author = dc.read("$.body.items[0].author");
+
+        assertThat(code).isEqualTo(1);
+        assertThat(title).isEqualTo("Junit");
+        assertThat(author).isEqualTo("겟인데어");
     }
 
 }
