c0936e4e59aef104d77012584424550c66c4b7b1
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-10-21 16:23:59 +0900

    Controller Integration Test - create book

--- src/test/java/com/study/junitproject/web/BookApiControllerTest.java
+++ src/test/java/com/study/junitproject/web/BookApiControllerTest.java
@@ -1,4 +1,61 @@
 package com.study.junitproject.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import com.study.junitproject.service.BookService;
+import com.study.junitproject.web.dto.request.BookSaveRequestDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BookApiControllerTest {
+
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private TestRestTemplate rt;
+
+    private static ObjectMapper om;
+    private static HttpHeaders headers;
+
+    @BeforeAll
+    public static void init() {
+        om = new ObjectMapper();
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Test
+    @DisplayName("책 등록하기")
+    void create() throws Exception{
+        //given
+        BookSaveRequestDto bookSaveRequestDto = new BookSaveRequestDto();
+        bookSaveRequestDto.setTitle("Testing...");
+        bookSaveRequestDto.setAuthor("Tester");
+
+        String body = om.writeValueAsString(bookSaveRequestDto);
+
+        //when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = rt.exchange("/api/v1/book", HttpMethod.POST, request, String.class);
+
+        //then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        String title = dc.read("$.body.title");
+        String author = dc.read("$.body.author");
+
+        assertThat(title).isEqualTo("Testing...");
+        assertThat(author).isEqualTo("Tester");
+    }
+
 }
